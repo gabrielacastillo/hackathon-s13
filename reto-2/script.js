4a37343282c019123f67d2c1cdcd254f617bc80f
Gabriela Castillo <gabrielacastillovas@gmail.com>
2020-06-27 09:03:35 -0500

    reto 2

--- reto-2/script.js
+++ reto-2/script.js
@@ -1,38 +1,105 @@
+//Global Variables
+let deleteElement;
+
 //Tools
-htmlItem = (value) => {
-    const item = document.createElement('div');
-    item.className = 'list__item';
+htmlItem = (nombre, raza, edad) => {
+    const item = document.createElement('tr');
     item.innerHTML = `
-        <input type="checkbox" class="item__check">
-            <label class="item__value">${value}</label>
-            <a class="item__delete" href="#">
+        <td>${nombre}</td>
+        <td>${raza}</td>
+        <td>${edad}</td>
+        <td>
+            <a class="body__edit" href="#">
+                <i class="fa fa-pencil fa-lg" aria-hidden="true"></i>
+            </a>
+        </td>
+        <td>
+            <a class="body__delete" href="#">
                 <i class="fa fa-trash-o fa-lg" aria-hidden="true"></i>
             </a>
-        <hr class="item_hr">
+        </td>
     `;
     return item;
 } 
 
 //Funtions from Events
-addItem = e => {
-    let value = document.getElementById("item_value").value;
-    let list = document.querySelector(".todo__list");
-    list.appendChild(htmlItem(value));
-    document.getElementById("item_value").value="";
+saveItem = e => {
+    let nombre = document.getElementById("input_nombre").value;
+    let raza = document.getElementById("input_raza").value;
+    let edad = document.getElementById("input_edad").value;
+
+    let list = document.querySelector(".table__body");
+    list.appendChild(htmlItem(nombre, raza, edad));
+    cleanInputs();
 };
 
-filterItem = e => {
-    alert("T_T Me falto Tiempo para Implementar esto T_T");
-};
 
-deleteItem = e => {
-    if(e.target.tagName == "I"){
-        let list = document.querySelector(".todo__list");
-        list.removeChild(e.target.parentElement.parentElement);
+
+deleteItemProposal = e => {
+    if(e.target.tagName == "I" && e.target.parentElement.classList.contains("body__delete")){
+        deleteElement = e.target.parentElement.parentElement.parentElement;
     }
 };
 
+deleteItem = e => {
+    let list = document.querySelector(".table__body");
+    list.removeChild(deleteElement);
+};
+
+
+//Tools
+cleanInputs = () => {
+    document.getElementById("input_nombre").value = "";
+    document.getElementById("input_raza").value = "";
+    document.getElementById("input_edad").value = "";
+}
+
 //Add Events
-document.querySelector("#add").addEventListener("click",addItem);
-document.querySelector("#fil").addEventListener("click",filterItem);
-document.querySelector(".todo__list").addEventListener("click",deleteItem);
+document.querySelector("#save").addEventListener("click",saveItem);
+document.querySelector(".table__body").addEventListener("click",deleteItemProposal);
+document.querySelector("#modal_aceptar").addEventListener("click",deleteItem);
+
+
+
+
+
+
+
+
+
+//Funcion Constructora
+function GabyModal(modalId) {
+    this.modalId = modalId;
+    this.init = function () {
+      const questions = document.querySelectorAll(".question");
+  
+      // Click function for show the Modal
+      document.querySelectorAll(".show").forEach((e) => {
+        e.addEventListener("click", function () {
+          document.querySelector(".mask").classList.add("active");
+        });
+      });
+  
+      // Function for close the Modal
+      function closeModal() {
+        document.querySelector(".mask").classList.remove("active");
+      }
+  
+      // Call the closeModal function on the clicks/keyboard
+      let list = document.querySelectorAll(".close, .mask, .aceptar, .cancelar");
+      list.forEach((e) => {
+        e.addEventListener("click", function () {
+          closeModal();
+        });
+      });
+  
+      document.addEventListener("keyup", function (e) {
+        if (e.keyCode == 27) {
+          closeModal();
+        }
+      });
+    };
+  }
+  
+  let gabyModal = new GabyModal("Accordion1");
+  gabyModal.init();
