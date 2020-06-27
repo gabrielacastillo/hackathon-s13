e51ce496abb30231f3f1d1ea12c42f6528dcc630
Gabriela Castillo <gabrielacastillovas@gmail.com>
2020-06-27 07:05:42 -0500

    XD

--- reto-2/script.js
+++ reto-2/script.js
@@ -1,1 +1,38 @@
+//Tools
+htmlItem = (value) => {
+    const item = document.createElement('div');
+    item.className = 'list__item';
+    item.innerHTML = `
+        <input type="checkbox" class="item__check">
+            <label class="item__value">${value}</label>
+            <a class="item__delete" href="#">
+                <i class="fa fa-trash-o fa-lg" aria-hidden="true"></i>
+            </a>
+        <hr class="item_hr">
+    `;
+    return item;
+} 
 
+//Funtions from Events
+addItem = e => {
+    let value = document.getElementById("item_value").value;
+    let list = document.querySelector(".todo__list");
+    list.appendChild(htmlItem(value));
+    document.getElementById("item_value").value="";
+};
+
+filterItem = e => {
+    alert("T_T Me falto Tiempo para Implementar esto T_T");
+};
+
+deleteItem = e => {
+    if(e.target.tagName == "I"){
+        let list = document.querySelector(".todo__list");
+        list.removeChild(e.target.parentElement.parentElement);
+    }
+};
+
+//Add Events
+document.querySelector("#add").addEventListener("click",addItem);
+document.querySelector("#fil").addEventListener("click",filterItem);
+document.querySelector(".todo__list").addEventListener("click",deleteItem);
